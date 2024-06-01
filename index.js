1926edf21b7a0c9bdd29ee17d207d001fa452479
GabrieldaCostaRodrigues <167185964+GRodrigues10@users.noreply.github.com>
2024-06-01 15:07:40 -0300

    Update

---
+++ index.js
@@ -0,0 +1,40 @@
+let input = document.getElementById('input');
+let resultado = document.getElementById('res');
+let conversor = '';
+
+function encriptar() {
+    if (input.value.length === 0) {
+        alert('Por favor, insira uma mensagem!');
+    } else {
+        conversor = ''; // Limpa a variável conversor
+        for (let i = 0; i < input.value.length; i++) {
+            let unicode = input.value.charCodeAt(i);
+            let novoUnicode = unicode + 3; // Incrementa 3 ao código Unicode
+            let novoCharCode = String.fromCharCode(novoUnicode);
+            
+            conversor += novoCharCode;
+        }
+        resultado.innerHTML = conversor;
+    }
+}
+
+function desencriptar() {
+    if (conversor.length === 0) {
+        alert('Por favor, primeiro criptografe uma mensagem!');
+    } else {
+        let mensagemDescriptografada = '';
+        for (let i = 0; i < conversor.length; i++) {
+            let unicode = conversor.charCodeAt(i);
+            let novoUnicode = unicode - 3; // Decrementa 3 do código Unicode
+            let novoCharCode = String.fromCharCode(novoUnicode);
+            
+            mensagemDescriptografada += novoCharCode;
+        }
+        resultado.innerHTML = mensagemDescriptografada;
+    }
+}
+
+function limpar() {
+    input.value = ''; // Limpa o input
+    resultado.innerHTML = 'Preencha os dados a caixa acima para encriptar uma mensagem!'; // Limpa o resultado
+}
